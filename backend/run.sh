9339b683ea3a22e7be28e937cc11b068962fc746
Shivam Agrawal <shivamagrawal@Shivams-MacBook-Air.local>
2024-10-13 00:07:20 -0700

    forgot to add backend part of refresh script flow

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 source .venv/bin/activate
 
 # Make sure to run on 0.0.0.0 so accessible from outside, not just localhost
-uvicorn server:app --host 0.0.0.0 --port 8000 --reload
+nohup uvicorn server:app --host 0.0.0.0 --port 8000 --reload > uvicorn.log 2>&1 &
