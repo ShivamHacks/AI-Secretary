cd1ef95bb63e5df275a2cdb9dd5742e1256b627e
Shivam Agrawal <12569537+ShivamHacks@users.noreply.github.com>
2024-11-08 22:54:01 -0800

    fixed websocket connection design: automatically tries reconnecting if connection broken. Creates new socket if isLocalhost changed

--- frontend/src/components/DataProvider.js
+++ frontend/src/components/DataProvider.js
@@ -6,10 +6,16 @@ const DataContext = createContext();
 const SAMPLE_LOCAL_DATA = require('./local_data_example.json');
 const USE_LOCAL_DATA = false;
 
+const ConnectionState = {
+  NOT_CONNECTED: 'NOT_CONNECTED',
+  CONNECTING: 'CONNECTING',
+  CONNECTED: 'CONNECTED'
+};
+
 export const DataProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
   const [data, setData] = useState({ chat: [], events: [], todo: [] });
-  const [isConnected, setIsConnected] = useState(false);
+  const [connectionState, setConnectionState] = useState(ConnectionState.NOT_CONNECTED);
   const socketRef = useRef(null);
 
   const isWindowLocalhost = window.location.hostname === "localhost";
@@ -47,76 +53,92 @@ export const DataProvider = ({ children }) => {
   const connectToSocket = () => {
     if (!userInfo) {
       console.log("User is not logged in");
-      return false;
+      return;
     }
     console.log("Connecting to WebSocket, isLocalhost:", isLocalhost);
+    setConnectionState(ConnectionState.CONNECTING);
     socketRef.current = new WebSocket(getWebSocketUrl(userInfo.email, userInfo.accessToken));
-    return true;
+
+    socketRef.current.onopen = () => {
+      console.log("WebSocket connected");
+      setConnectionState(ConnectionState.CONNECTED);
+    };
+
+    socketRef.current.onmessage = (event) => {
+      try {
+        const receivedData = JSON.parse(event.data);
+        if (receivedData.type === 'chunk') {
+          // Handle the streamed message chunk (partial message)
+          setData((prevData) => {
+            // Find the last message and update its content if it's partial
+            const updatedChat = [...prevData.chat];
+            const lastMessageIndex = updatedChat.length - 1;
+            if (lastMessageIndex == 0) {
+              return prevData;
+            }
+            updatedChat[lastMessageIndex].content += receivedData.chunk;
+            return {
+              ...prevData,
+              chat: updatedChat
+            };
+          });
+        } else if (receivedData.type === 'final') {
+          // Handle the final updated data after the stream is done
+          setData(() => {
+            return receivedData.data;
+          });
+        } else if (receivedData.type === 'feedback_confirmation') {
+          alert(`Feedback submitted: ${receivedData.feedback}`)
+        } else {
+          console.error("Received unknown data type");
+        }
+      } catch (err) {
+        console.error("Error parsing WebSocket message:", err);
+      }
+    };
+
+    socketRef.current.onerror = (error) => {
+      console.error("WebSocket error:", error);
+      setConnectionState(ConnectionState.NOT_CONNECTED);
+    };
+
+    socketRef.current.onclose = () => {
+      console.log("WebSocket connection closed");
+      setConnectionState(ConnectionState.NOT_CONNECTED);
+    };
   };
 
+  // If the user info or isLocalhost changes, close the socket
   useEffect(() => {
-    if (USE_LOCAL_DATA) {
-      setData(SAMPLE_LOCAL_DATA);
-      setIsConnected(false); // No connection, since local data is used
-    } else {
-      if (!connectToSocket()) {
-        console.log("WebSocket connection not established");
-        return;
-      }
-
-      socketRef.current.onopen = () => {
-        console.log("WebSocket connected");
-        setIsConnected(true);
-      };
-
-      socketRef.current.onmessage = (event) => {
-        try {
-          const receivedData = JSON.parse(event.data);
-          if (receivedData.type === 'chunk') {
-            // Handle the streamed message chunk (partial message)
-            setData((prevData) => {
-              // Find the last message and update its content if it's partial
-              const updatedChat = [...prevData.chat];
-              const lastMessageIndex = updatedChat.length - 1;
-              if (lastMessageIndex == 0) {
-                return prevData;
-              }
-              updatedChat[lastMessageIndex].content += receivedData.chunk;
-              return {
-                ...prevData,
-                chat: updatedChat
-              };
-            });
-          } else if (receivedData.type === 'final') {
-            // Handle the final updated data after the stream is done
-            setData(() => {
-              return receivedData.data;
-            });
-          } else if (receivedData.type === 'feedback_confirmation') {
-            alert(`Feedback submitted: ${receivedData.feedback}`)
-          } else {
-            console.error("Received unknown data type");
-          }
-        } catch (err) {
-          console.error("Error parsing WebSocket message:", err);
-        }
-      };
-
-      socketRef.current.onerror = (error) => {
-        console.error("WebSocket error:", error);
-      };
-
-      socketRef.current.onclose = () => {
-        console.log("WebSocket connection closed");
-        setIsConnected(false);
-      };
-
-      return () => {
-        console.log("State changed, closing websocket");
-        socketRef.current.close();
-      };
+    if (!socketRef.current) {
+      return;
     }
-  }, [userInfo, isLocalhost]);
+    console.log("Closing websocket to change isLocalhost");
+    socketRef.current.close();
+  }, [isLocalhost]);
+
+  // Reconnect to the socket if it gets disconnected
+  useEffect(() => {
+    console.log("Connection state changed:", connectionState);
+    if (connectionState !== ConnectionState.NOT_CONNECTED) {
+      return;
+    }
+    connectToSocket();
+  }, [connectionState, userInfo]);
+
+  // Retry connection periodically if not connected
+  useEffect(() => {
+    if (connectionState !== ConnectionState.NOT_CONNECTED || !userInfo) {
+      return;
+    }
+
+    const retryInterval = setInterval(() => {
+      console.log("Attempting to reconnect WebSocket...");
+      connectToSocket();
+    }, 500);
+
+    return () => clearInterval(retryInterval);
+  }, [connectionState, userInfo]);
 
   const addMessage = (message) => {
     setData((prevData) => {
@@ -129,7 +151,7 @@ export const DataProvider = ({ children }) => {
         content: "",
       }];
       // Send the message to the server if connected
-      if (socketRef.current && isConnected) {
+      if (socketRef.current && connectionState === ConnectionState.CONNECTED) {
         const cujID = logCreateMessageEvent(userInfo.email);
         const messagePayload = JSON.stringify({
           newMessage: message,
@@ -149,7 +171,7 @@ export const DataProvider = ({ children }) => {
   };
 
   const sendFeedback = (feedback) => {
-    if (socketRef.current && isConnected) {
+    if (socketRef.current && connectionState === ConnectionState.CONNECTED) {
       const feedbackPayload = JSON.stringify({ feedback: feedback });
       console.log("Sending feedback to server:", feedbackPayload);
       socketRef.current.send(feedbackPayload);
